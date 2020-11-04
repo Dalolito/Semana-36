d7191ec4f0a1722434b0db3e4248768828d13f6c
Dalolito <70274865+Dalolito@users.noreply.github.com>
2020-11-04 08:07:57 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+var libro=document.getElementById('libro');
+var imglibro=document.getElementById('imglibro');
+var bienvenida=document.getElementById('bienvenida');
+var flecha=document.getElementById('imgflecha');
+var pag1=document.getElementById('pag1');
+var imglibroabierto=document.getElementById('imglibroabierto');
+var narrador=document.getElementById('narrador');
+var pagina1=function () {
+	libro.className="bloqueado";
+		pag1.className="pag1";
+
+	
+
+
+
+}
